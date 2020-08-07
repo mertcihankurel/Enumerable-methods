ed2d188e767ee5b3767db110d009481f3971cb06
cristianCeamatu <cristian.ceamatu@gmail.com>
2020-08-07 17:21:12 +0300

    Added my_select method

--- main.rb
+++ main.rb
@@ -17,10 +17,28 @@ module Enumerable
 
     self
   end
+
+  def my_select
+    return to_enum(:select) unless block_given?
+
+    array = to_a
+    output_array = []
+
+    array.my_each { |element| output_array << element if yield(element) }
+    if self.class == Hash
+      output_array = Hash[output_array.map { |key, value| [key, value] }]
+    end
+    output_array
+  end
   
 end
 
+test_hash = {
+  "x": 1,
+  "Y": 2,
+  "z": 3,
+  "i": 2,
+}
 
-
-[1, 2, 4, 6].my_each_with_index { |element, index| p "#{element} * #{index}"}
+p test_hash.my_select { |symbol, value| symbol == :Y }
 # p [1, 2, 4, 6, 5].my_each_with_index
