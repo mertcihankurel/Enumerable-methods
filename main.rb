009b64c00aa6925c011169cfec143d85edc61b37
Muhammed Cihad Kurel <mcihadkurel@gmail.com>
2020-08-07 17:04:01 +0300

    My_each_with_index def added

--- main.rb
+++ main.rb
@@ -8,10 +8,19 @@ module Enumerable
 
     self
   end
+  
+  def my_each_with_index
+    return to_enum unless block_given?
+    
+    array = to_a
+    array.size.times { |index| yield(array[index], index) }
 
+    self
+  end
   
 end
 
 
-# [1, 2, 4, 6].each { |element| p element * 3}
-p [1, 2, 4, 6, 5].my_each
+
+[1, 2, 4, 6].my_each_with_index { |element, index| p "#{element} * #{index}"}
+# p [1, 2, 4, 6, 5].my_each_with_index
