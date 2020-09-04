bcab888932b3b01eaf31b574f74ee656f059f017
Muhammed Cihad Kurel <mcihadkurel@gmail.com>
2020-09-04 16:23:35 +0300

    Create tests

--- spec/enumerables_testing.rb
+++ spec/enumerables_testing.rb
@@ -6,9 +6,27 @@ describe Enumerable do
     let(:my_string_array) {%w[Marc Luc Jean]}
     let(:my_numric_array) {[1, 5i, 5.67]}
 
-    describe "#my_all?" do 
+    describe '#my_all?' do 
         it "Check if array pass all arguments" do
         expect(my_array.my_all?(3)).to eql(false)
+        end
+    end
+
+    describe '#my_any?' do
+        it "Check if array pass any arguments" do
+        expect(my_array.my_any?(3)).to eql(true)
+        end
+    end
+
+    describe '#my_none?' do
+        it "Check if array pass any arguments" do
+        expect(my_array.my_none?(3)).to eql(false)
+        end
+    end
+
+    describe '#my_select' do
+        it "Check if array pass any arguments" do
+        expect(my_array.my_select {|n| n.odd?}).to eql(my_array)
         end
     end
 end
