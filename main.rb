893a66789fd0101e2095688511e4e49290394ae8
mahmoud717 <mahmoudmohammad717@gmail.com>
2020-09-04 16:41:43 +0200

    finished all edge test cases

--- main.rb
+++ main.rb
@@ -1,139 +1,166 @@
 # Personalized methods for the Enumerable module
 module Enumerable
-  def my_each
+  def my_each(_arr = nil)
     return to_enum unless block_given?
 
-    array = to_a
-    array.size.times { |index| yield(array[index]) }
-
+    Array(self).length.times do| i|
+      yield(Array(self)[i])
+    end
     self
   end
 
   def my_each_with_index
-    return to_enum unless block_given?
+    return to_enum(:my_each_with_index) unless block_given?
 
-    array = to_a
-    array.size.times { |index| yield(array[index], index) }
-
+    Array(self).length.times do |index|
+      yield(Array(self)[index], index)
+    end
     self
   end
 
   def my_select
-    return to_enum(:select) unless block_given?
+    return to_enum(:my_select) unless block_given?
 
-    array = to_a
-    output_array = []
-
-    array.my_each { |element| output_array << element if yield(element) }
-
-    output_array = Hash[output_array.map { |key, value| [key, value] }] if self.class == Hash
-
-    output_array
+    outcome = []
+    my_each { |i| outcome.push(i) if yield(i) }
+    outcome
   end
 
   def my_all?(arg = nil)
-    array = to_a
-
-    if arg.nil?
-      array.my_each { |element| return false unless element } unless block_given?
-
-      array.my_each { |element| return false unless yield(element) } if block_given?
-
-    elsif !arg.nil?
-      array.my_each { |element| return false if !element.is_a?(String) || !element.match(arg) } if arg.is_a?(Regexp)
-
-      array.my_each { |element| return false unless element.is_a?(arg) } if arg.is_a?(Class)
-
-      array.my_each { |element| return false if element != arg } unless arg.is_a?(Class) || arg.is_a?(Regexp)
+    if arg.nil? && !block_given?
+      output = true
+      my_each { |item| output = false if !item || item.nil? }
+      output
+    elsif block_given?
+      outcome = true
+      my_each { |item| outcome = false unless yield(item) }
+      outcome
+    elsif arg.is_a?(Class)
+      outcome = true
+      my_each { |item| outcome = false unless item.is_a?(arg) }
+      outcome
+    elsif arg.is_a?(Integer)
+      outcome = true
+      my_each { |item| outcome = false if item != arg }
+      outcome
+    elsif arg.is_a?(String) || arg.is_a?(Regexp)
+      outcome = true
+      my_each { |item| outcome = false unless item.match?(arg) }
+      outcome
     end
-
-    true
   end
 
   def my_any?(arg = nil)
-    array = to_a
+    if arg.nil? && !block_given?
+      output = false
+      my_each { |item| output = true if item && !item.nil? }
+      output
+    elsif block_given?
+      outcome = false
+      my_each { |item| outcome = true if yield(item) }
+      outcome
+    elsif arg.is_a?(Class)
+      outcome = false
+      my_each { |item| outcome = true if item.is_a?(arg) }
+      outcome
+    elsif arg.is_a?(Integer)
+      outcome = false
+      my_each { |item| outcome = true if item == arg }
+      outcome
+    elsif arg.is_a?(String) || arg.is_a?(Regexp)
+      outcome = false
+      my_each { |item| outcome = true if item.match?(arg) }
+      outcome
 
-    if arg.nil?
-      array.my_each { |element| return true if element } unless block_given?
-
-      array.my_each { |element| return true if yield(element) } if block_given?
-
-    elsif !arg.nil?
-
-      array.my_each { |element| return true if element.is_a?(String) && element.match(arg) } if arg.is_a?(Regexp)
-
-      array.my_each { |element| return true if element.is_a?(arg) } if arg.is_a?(Class)
-
-      array.my_each { |element| return true if element == arg } unless arg.is_a?(Class) && arg.is_a?(Regexp)
     end
-
-    false
   end
 
   def my_none?(arg = nil)
-    array = to_a
-
-    if arg.nil?
-      array.my_each { |element| return false if element } unless block_given?
-
-      array.my_each { |element| return false if yield(element) } if block_given?
-
-    elsif !arg.nil?
-      array.my_each { |element| return false if element.is_a?(String) && element.match(arg) } if arg.is_a?(Regexp)
-
-      array.my_each { |element| return false if element.is_a?(arg) } if arg.is_a?(Class)
-
-      array.my_each { |element| return false if element == arg } unless arg.is_a?(Class) && arg.is_a?(Regexp)
+    if arg.nil? && !block_given?
+      output = true
+      my_each { |item| output = false if item && !item.nil? }
+      output
+    elsif block_given?
+      outcome = true
+      my_each { |item| outcome = false if yield(item) }
+      outcome
+    elsif arg.is_a?(Class)
+      outcome = true
+      my_each { |item| outcome = false if item.is_a?(arg) }
+      outcome
+    elsif arg.is_a?(Integer)
+      outcome = true
+      my_each { |item| outcome = false if item == arg }
+      outcome
+    elsif arg.is_a?(String) || arg.is_a?(Regexp)
+      outcome = true
+      my_each { |item| outcome = false if item.match?(arg) }
+      outcome
     end
-
-    true
   end
 
   def my_count(arg = nil)
-    array = to_a
-    count = 0
-
-    if arg.nil?
-      block_given? ? array.my_each { |element| count += 1 if yield(element) } : array.my_each { count += 1 }
-    else
-      array.my_each { |element| count += 1 if element == arg }
+    n = 0
+    if block_given?
+      my_each { |item| n += 1 if yield(item) }
+    elsif arg.nil?
+      n += 1 while n < size
+    elsif arg.is_a?(Class)
+      my_each { |item| n += 1 if item.is_a?(arg) }
+    elsif arg.is_a?(Integer)
+      my_each { |item| n += 1 if item == arg }
+    else arg.is_a?(String)
+         my_each { |item| n += 1 if item == arg || item.match?(arg) }
     end
-
-    count
+    n
   end
 
-  def my_map(&_change_proc)
-    return to_enum(:map) unless block_given?
+  def my_map(arg = nil)
+    if arg.nil?
+      return to_enum unless block_given?
 
-    array = to_a
-    output_array = []
+      output = []
+      my_each { |item| output.push(yield(item)) }
+      output
+    elsif arg.class == Proc
+      my_each { |item| Proc(item) if item }
+    elsif arg.is_a?(Class)
+      output = []
+      my_each { |item| output.push(item) if item.is_a?(arg) }
+      output
+    elsif arg.is_a?(Integer)
+      output = []
+      my_each { |item| output.push(item) if item == arg }
+      output
+    else arg.is_a?(String)
+         output = []
+         my_each { |item| output.push(item) if item == arg || item.match?(arg) }
+         output
 
-    array.my_each { |element| output_array << yield(element) }
-
-    output_array
+    end
+    output
   end
 
   def my_inject(*arg)
-    array = to_a
-    arg1 = arg[0]
-    arg2 = arg[1]
+    raise('LocalJumpError.new NO BLOCK OR ARGUMENT GIVEN!') if !block_given? && arg == nil?
 
-    both_args = arg1 && arg2
-    only_one_arg = arg1 && !arg2
-    no_arg = !arg1
+    check = false
+    result = Array(self)[0]
+    if (arg[0].class == Symbol) || arg[0].nil?
+      check = true
+    elsif arg[0].is_a? Numeric
+      result = arg[0]
+    end
+    Array(self).my_each_with_index do |item, index|
+      next if check && index.zero?
 
-    result = both_args || (only_one_arg && block_given?) ? arg1 : array.first
-
-    if block_given?
-      array.drop(1).my_each { |next_element| result = yield(result, next_element) } if no_arg
-
-      array.my_each { |next_element| result = yield(result, next_element) } if only_one_arg
-
-    else
-      array.drop(1).my_each { |next_element| result = result.send(arg1, next_element) } if only_one_arg
-
-      array.drop(1).my_each { |next_element| result = result.send(arg2, next_element) } if both_args
-
+      if block_given?
+        result = yield(result, item)
+      elsif arg[0].class == Symbol
+        result = result.send(arg[0], item)
+      elsif arg[0].is_a? Numeric
+        result = result.send(arg[1], item)
+      end
     end
     result
   end
